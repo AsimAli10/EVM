3b16b93006ba881f5718a62ff305f4bb7bdd70d3
Asim <Asim@Asim-PC>
2022-04-18 01:34:14 +0500

    tried updating login controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-			primaryStage.setTitle("Aeries");
+			primaryStage.setTitle("EVM");
 			primaryStage.setScene(new Scene(root));
 			primaryStage.show();
 		} catch(Exception e) {
